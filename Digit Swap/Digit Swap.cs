549b5d14ba59e035a12af9a1833d85922b55e17e
Cecilia Nguyen <91034023+t-cecilia-nguyen@users.noreply.github.com>
2023-10-26 15:20:37 -0400

    Create Digit Swap.cs

---
+++ Digit Swap/Digit Swap.cs
@@ -0,0 +1,9 @@
+using System;
+
+internal class Program {
+    static void Main(string[] args) {
+        string input = Console.ReadLine();
+        Console.Write(input[1]);
+        Console.Write(input[0]);
+    }
+}
