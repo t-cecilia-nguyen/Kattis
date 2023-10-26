9b94abbe3f041cd41b42df36feb9d7c03d1bb59b
Cecilia Nguyen <91034023+t-cecilia-nguyen@users.noreply.github.com>
2023-10-26 15:33:31 -0400

    Create R2.cs

---
+++ R2/R2.cs
@@ -0,0 +1,15 @@
+using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string[] inputs = input.Split(' ');
+        if (int.TryParse(inputs[0], out int result) 
+            && int.TryParse(inputs[1], out int result2))
+        {
+            Console.WriteLine(result2 * 2 - result);
+        }
+    }
+}
