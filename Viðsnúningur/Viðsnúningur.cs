4daf28ccd5914a13c796387e9aae9bf67989947f
Cecilia Nguyen <91034023+t-cecilia-nguyen@users.noreply.github.com>
2023-10-26 15:35:17 -0400

    Create Viðsnúningur.cs

---
+++ Viðsnúningur/Viðsnúningur.cs
@@ -0,0 +1,10 @@
+using System;
+
+internal class Program {
+    static void Main(string[] args) {
+        string input = Console.ReadLine();        
+        for (int i = input.Length - 1; i >= 0; i--) {
+            Console.Write(input[i]);
+        }
+    }
+}
