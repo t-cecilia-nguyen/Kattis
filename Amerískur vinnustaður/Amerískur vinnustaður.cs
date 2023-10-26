a1019afbef5e3bb9e6b73872d60c74aa87273412
Cecilia Nguyen <91034023+t-cecilia-nguyen@users.noreply.github.com>
2023-10-26 15:37:08 -0400

    Create Amerískur vinnustaður.cs

---
+++ Amerískur vinnustaður/Amerískur vinnustaður.cs
@@ -0,0 +1,10 @@
+using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        double input = Double.Parse(Console.ReadLine());
+        Console.WriteLine(input * 0.09144);
+    }
+}
