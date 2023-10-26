5b911ce33d0852c3edce5f1ff3a524902a293824
Cecilia Nguyen <91034023+t-cecilia-nguyen@users.noreply.github.com>
2023-10-26 15:37:35 -0400

    Create Óvissa.cs

---
+++ Óvissa/Óvissa.cs
@@ -0,0 +1,10 @@
+using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine(input.Length);
+    }
+}
