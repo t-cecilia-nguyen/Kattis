ad96d0a08eea09640d0c9ee0d8d2c09d915d0628
Cecilia Nguyen <91034023+t-cecilia-nguyen@users.noreply.github.com>
2023-10-26 15:36:47 -0400

    Create Framtíðar FIFA.cs

---
+++ Framtíðar FIFA/Framtíðar FIFA.cs
@@ -0,0 +1,11 @@
+using System;
+
+internal class Program {
+    static void Main(string[] args) {
+        int a = Int32.Parse( Console.ReadLine());
+        int b = Int32.Parse( Console.ReadLine());        
+        int c = a / b;
+        Console.WriteLine(2022 + c);
+        
+    }
+}
