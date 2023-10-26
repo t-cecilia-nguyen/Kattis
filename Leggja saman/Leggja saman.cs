ba83a4bb1d3f61bb97b4f27cf7b1dc8a951795ea
Cecilia Nguyen <91034023+t-cecilia-nguyen@users.noreply.github.com>
2023-10-26 15:35:37 -0400

    Create Leggja saman.cs

---
+++ Leggja saman/Leggja saman.cs
@@ -0,0 +1,9 @@
+using System;
+
+internal class Program {
+    static void Main(string[] args) {
+        int a = Int32.Parse(Console.ReadLine());
+        int b = Int32.Parse(Console.ReadLine());        
+        Console.WriteLine(a + b);
+    }
+}
