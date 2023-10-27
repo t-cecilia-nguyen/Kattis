f306358cf15abf054b9616c58d915b0589f69b94
Cecilia Nguyen <91034023+t-cecilia-nguyen@users.noreply.github.com>
2023-10-27 16:36:28 -0400

    Create I've Been Everywhere, Man.cs

---
+++ I've Been Everywhere, Man/I've Been Everywhere, Man.cs
@@ -0,0 +1,42 @@
+using System;
+
+internal class Program {
+    static void Main(string[] args) {
+        int testCases = Int32.Parse(Console.ReadLine());
+        int[] testCasesOutput = new int[testCases];
+        for (int i = 0; i < testCases; i++)
+        {
+            int trips = Int32.Parse(Console.ReadLine());
+            string[] cities = new string[trips];
+            // City input
+            for (int l = 0; l < cities.Length; l++)
+            {
+                cities[l] = Console.ReadLine();
+            }
+
+            // Pick all elements one by one
+            int uniqueCities = 1;
+            for (int j = 1; j < cities.Length; j++)
+            {
+                int k = 0;
+                for (k = 0; k < j; k++)
+                {
+                    if (cities[j] == cities[k])
+                    {
+                        break;
+                    }
+                }
+                if (j == k)
+                {
+                    uniqueCities++;
+                }
+            }
+            testCasesOutput[i] = uniqueCities;
+            uniqueCities = 0;
+        }
+        for (int c = 0; c < testCasesOutput.Length; c++)
+        {
+            Console.WriteLine(testCasesOutput[c]);
+        }
+    }
+}
