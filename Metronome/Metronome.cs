4557b5ad1ba694470a364a92edc36952c8bd0fe0
Cecilia Nguyen <91034023+t-cecilia-nguyen@users.noreply.github.com>
2023-10-29 15:03:42 -0400

    Update Metronome.cs

--- Metronome/Metronome.cs
+++ Metronome/Metronome.cs
@@ -2,8 +2,6 @@ using System;
 
 internal class Program {
     static void Main(string[] args) {
-        double input = Double.Parse(Console.ReadLine());
-        double output = Math.Round(input / 4, 2);
-        Console.WriteLine(output);
+        Console.WriteLine(Double.Parse(Console.ReadLine()) / 4);
     }
 }
